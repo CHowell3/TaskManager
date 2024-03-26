4483217b4396aba79b7e61a049adc932beb5cea7
Rachel Cantrell <rmcantre@ncsu.edu>
2024-03-26 16:24:34 -0400

    [Skeleton] Added comments and method stubs to NotebookReader

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReader.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReader.java
@@ -1,5 +1,47 @@
 package edu.ncsu.csc216.wolf_tasks.model.io;
 
-public class NotebookReader {
+import java.io.File;
 
+import edu.ncsu.csc216.wolf_tasks.model.notebook.Notebook;
+import edu.ncsu.csc216.wolf_tasks.model.tasks.AbstractTaskList;
+import edu.ncsu.csc216.wolf_tasks.model.tasks.Task;
+import edu.ncsu.csc216.wolf_tasks.model.tasks.TaskList;
+
+public class NotebookReader {
+	
+	/**
+	 * Constructor for NotebookReader. Since all methods are static, this should not usually be used.
+	 */
+	public NotebookReader() {}
+	
+	/**
+	 * Loads a Notebook object from the file provided.
+	 * @param file the file to load from
+	 * @return the Notebook loaded from the file
+	 */
+	public static Notebook readNotebookFile(File file) {
+		processTaskList("");
+		return null;
+	}
+	
+	/**
+	 * Private helper method for processing a taskList.
+	 * @param taskListString the string to be processed
+	 * @return the TaskList
+	 */
+	private static TaskList processTaskList(String taskListString){
+		processTask(null, "");
+		return null;
+	}
+	
+	/**
+	 * Private helper method for processing a task.
+	 * @param taskList the taskList of the task being processed
+	 * @param taskString the string to be processed
+	 * @return the Task
+	 */
+	private static Task processTask(AbstractTaskList taskList, String taskString) {
+		return null;
+	}
+		
 }
