56fe4224902ee3346e34d86ca1c27c0160b604de
Rachel Cantrell <rmcantre@ncsu.edu>
2024-03-26 16:01:45 -0400

    [Skeleton] Comments and method stubs for TaskList and ActiveTaskList

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/tasks/ActiveTaskList.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/tasks/ActiveTaskList.java
@@ -1,5 +1,55 @@
 package edu.ncsu.csc216.wolf_tasks.model.tasks;
 
 public class ActiveTaskList extends AbstractTaskList {
+	
+	/**
+	 * "Active Tasks" - the name of the Active Tasks list
+	 */
+	public static final String ACTIVE_TASKS_NAME = "Active Tasks";
+
+	/**
+	 * Constructor for ActiveTaskList. Calls the super constructor with the expected name and a completed count of 0. 
+	 */
+	public ActiveTaskList() {
+		super(ACTIVE_TASKS_NAME, 0);
+	}
+
+	/**
+	 * Checks if the task is active before adding it to this list.
+	 * Throws an IllegalArgumentException "Cannot add task to Active Tasks" if the task is not active.
+	 * @throws IllegalArgumentException if task is not active
+	 */
+	@Override
+	public void addTask(Task t) {
+		
+	}
+	
+	/**
+	 * Ensures the paramater value matches the expected name.
+	 * If the parameter isn't the expected value, an IllegalArgumentException is thrown with the message "The Active Tasks list may not be edited."
+	 * @throws IllegalArgumentException if the parameter doesn't match the expected value
+	 */
+	@Override
+	public void setTaskListName(String s) {
+		
+	}
+	
+	/**
+	 * Returns a 2D String array for display.
+	 * The first column is the name of the first TaskList the task belongs to.
+	 * The second column is the names of the tasks.
+	 * @return the array for display
+	 */
+	@Override
+	public String[][] getTasksAsArray() {
+		return null;
+	}
+	
+	/**
+	 * Clears the Active Tasks list of all tasks.
+	 */
+	public void clearTasks() {
+		
+	}
 
 }
--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/tasks/TaskList.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/tasks/TaskList.java
@@ -1,5 +1,39 @@
 package edu.ncsu.csc216.wolf_tasks.model.tasks;
 
-public class TaskList extends AbstractTaskList {
+/**
+ * This class models a standard task list, with a variable name. It's a Child class of AbstractTaskList.
+ * Holds a list of tasks in an ISwapList.
+ * @author Rachel Cantrell
+ */
+public class TaskList extends AbstractTaskList implements Comparable<TaskList>{
 
+	/**
+	 * Constructor for TaskList. Calls the constructor of the superclass and does nothing else.
+	 * @param taskListName the name of the taskList
+	 * @param completedCount the number of tasks completed in this taskList
+	 */
+	public TaskList(String taskListName, int completedCount) {
+		super(taskListName, completedCount);
+	}
+
+	/**
+	 * Returns a 2D String array for display.
+	 * The first column is the priorities of the tasks, starting at 1.
+	 * The second column is the names of the tasks.
+	 * @return the array for display
+	 */
+	@Override
+	public String[][] getTasksAsArray() {
+		return null;
+	}
+
+	/**
+	 * Compares the taskLists by name. Returns an integer representing the difference between them.
+	 * If the returned value is zero, the taskLists are considered equal.
+	 * @param taskList the taskList to be compared
+	 * @return int representing the difference between the taskLists
+	 */
+	public int compareTo(TaskList taskList) {
+		return this.getTaskListName().compareTo(taskList.getTaskListName());
+	}
 }
