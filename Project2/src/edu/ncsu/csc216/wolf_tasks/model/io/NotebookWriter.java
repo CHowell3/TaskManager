2ce6c7406ef83fda58c9f5ab242aeef0018d2000
Rachel Cantrell <rmcantre@ncsu.edu>
2024-03-26 16:16:06 -0400

    [Skeleton] Added comments and method stubs for NotebookReader class.

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookWriter.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookWriter.java
@@ -1,5 +1,27 @@
 package edu.ncsu.csc216.wolf_tasks.model.io;
 
+import java.io.File;
+
+import edu.ncsu.csc216.wolf_tasks.model.tasks.TaskList;
+import edu.ncsu.csc216.wolf_tasks.model.util.ISortedList;
+
 public class NotebookWriter {
+	
+	/**
+	 * Constructor for NotebookWriter. Since all methods are static, this should not usually be used.
+	 */
+	public NotebookWriter() {}
+
+	/**
+	 * Writes a Notebook to a file. Uses the Task.toString() method.
+	 * If there are any errors, an IllegalArgumentException is thrown with the message "Unable to save file."
+	 * @param file the file to write to
+	 * @param notebookName the name of the notebook name saved
+	 * @param taskLists the TaskLists in the notebook being saved
+	 * @throws IllegalArgumentException in the case of any errors or exceptions
+	 */
+	public static void writeNotebookFile(File file, String notebookName, ISortedList<TaskList> taskLists) {
+		
+	}
 
 }
