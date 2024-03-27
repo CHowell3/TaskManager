0b3beba3f98204139402ad8e825e1b523d01d81d
jchowell <jchowell@ncsu.edu>
2024-03-27 17:44:01 -0400

    Skeleton of notebook class

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/notebook/Notebook.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/notebook/Notebook.java
@@ -1,5 +1,76 @@
 package edu.ncsu.csc216.wolf_tasks.model.notebook;
 
-public class Notebook {
+import java.io.File;
 
+import edu.ncsu.csc216.wolf_tasks.model.tasks.AbstractTaskList;
+import edu.ncsu.csc216.wolf_tasks.model.tasks.Task;
+import edu.ncsu.csc216.wolf_tasks.model.tasks.TaskList;
+
+public class Notebook {
+	
+	private String notebookName;
+	
+	private boolean isChanged;
+	
+	
+	public Notebook(String name) {
+		
+	}
+	
+	public void saveNotebook(File file) {
+		
+	}
+	
+	public String getNotebookName() {
+		return "";
+	}
+
+	private void setNotebookName(String name) {
+		
+	}
+	
+	public boolean isChanged() {
+		return true;
+	}
+	
+	public void setChanged(boolean b) {
+		
+	}
+	
+	public void addTaskList(TaskList t) {
+		
+	}
+	
+	public String[] getTaskListsNames() {
+		return null;
+	}
+	
+	private void getActiveTaskList() {
+		
+	}
+	
+	public void setCurrentTaskList(String list) {
+		
+	}
+	
+	public AbstractTaskList getCurrentTaskList() {
+		return null;
+	}
+	
+	public void editTaskList(String list) {
+		
+	}
+	
+	public void removeTaskList(String list) {
+		
+	}
+	
+	public void addTask(Task t) {
+		
+	}
+	
+	public void editTask(int x, String s, String y, boolean b, boolean p) {
+		
+	}
 }
+
