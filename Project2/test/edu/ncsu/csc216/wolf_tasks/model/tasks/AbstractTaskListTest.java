42be6c2985e5d246775be71de338ff6850f25a7a
Rachel Cantrell <rmcantre@ncsu.edu>
2024-03-27 20:56:06 -0400

    [Comments] Added comments to Test methods
    Tests were of were NotebookReader, NotebookWriter, Notebook, and
    AbstractTaskList

--- Project2/test/edu/ncsu/csc216/wolf_tasks/model/tasks/AbstractTaskListTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/tasks/AbstractTaskListTest.java
@@ -4,54 +4,86 @@ import static org.junit.Assert.fail;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Test for AbstractTaskList class. Since AbstractTaskList is an abstract class, most of the tests are done through one of its child classes.
+ */
 public class AbstractTaskListTest {
 
-	
+	/**
+	 * Tests the AbstractTaskList constructor.
+	 */
 	@Test
 	public void testAbstractTaskListConstructor() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the getTaskListName method
+	 */
 	@Test
 	public void getTaskListNameTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the setTaskListName method
+	 */
 	@Test
 	public void setTaskListNameTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the getTasks method
+	 */
 	@Test
 	public void getTasksTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the getCompletedCount method
+	 */
 	@Test
 	public void getCompletedCountTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the addTask method
+	 */
 	@Test
 	public void addTaskTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the removeTask method
+	 */
 	@Test
 	public void removeTaskTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the getTask method
+	 */
 	@Test
 	public void getTaskTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the completeTask method
+	 */
 	@Test
 	public void completeTaskTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the getTasksAsArray method
+	 */
 	@Test
 	public void getTasksAsArrayTest() {
 		fail("Not yet implemented");
