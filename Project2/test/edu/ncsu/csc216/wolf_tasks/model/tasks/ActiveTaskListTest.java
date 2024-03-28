f22dc2f742bd7ffd55f81ad20c659066236cd336
Rachel Cantrell <rmcantre@ncsu.edu>
2024-03-27 21:17:20 -0400

    [Javadoc] Added comments to the rest of the test classes

--- Project2/test/edu/ncsu/csc216/wolf_tasks/model/tasks/ActiveTaskListTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/tasks/ActiveTaskListTest.java
@@ -4,29 +4,46 @@ import static org.junit.Assert.fail;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Test for the ActiveTaskList class. Since ActiveTaskList is a child class of AbstractTaskList, AbstractTaskListTest must also be used to ensure full functionality.
+ */
 public class ActiveTaskListTest {
 
-
+	/**
+	 * Tests the ActiveTaskList constructor.
+	 */
 	@Test
 	public void testActiveTaskListConstructor() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the addTask method.
+	 */
 	@Test
 	public void addTaskTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the setTaskListName method.
+	 */
 	@Test
 	public void setTaskListNameTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the getTasksAsArray method.
+	 */
 	@Test
 	public void getTasksAsArrayTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the clearTasks method.
+	 */
 	@Test
 	public void clearTasksTest() {
 		fail("Not yet implemented");
