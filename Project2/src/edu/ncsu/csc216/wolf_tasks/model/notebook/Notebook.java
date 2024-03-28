0a8c82bac314c2d77251bf709c45a95d350a778d
jchowell <jchowell@ncsu.edu>
2024-03-27 21:18:28 -0400

    Added javadoc to classes: Notebook and SortedList

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/notebook/Notebook.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/notebook/Notebook.java
@@ -6,70 +6,150 @@ import edu.ncsu.csc216.wolf_tasks.model.tasks.AbstractTaskList;
 import edu.ncsu.csc216.wolf_tasks.model.tasks.Task;
 import edu.ncsu.csc216.wolf_tasks.model.tasks.TaskList;
 
+/**
+ * Notebook class that has an ISortedList of TaskLists, one ActiveTaskList, an AbstractTaskList for the currentTaskList,
+ * a notebookName, and a boolean flag that keeps track of if the Notebook has been changed since the last save. Methods include saving
+ * notebook to a file, setting its name, changing the active task list, editing tasks and more.
+ */
 public class Notebook {
 	
+	/** name of specific name */
 	private String notebookName;
 	
+	/** if task/taskList in notebook is changed */
 	private boolean isChanged;
 	
-	
+	/**
+	 * Constructor of notebook class that creates the notebook with specific name, initializes isChanged to true,
+	 * constructs the taskList to be a SortedList and activeTaskList is constructed and set to currentTaskList
+	 * @param name of the specific notebook
+	 */
 	public Notebook(String name) {
 		
 	}
 	
+	/**
+	 * Saves the current notebook to the given file. 
+	 * isChanged is updated to false
+	 * @param file used to save the notebook to
+	 */
 	public void saveNotebook(File file) {
 		
 	}
 	
+	/**
+	 * Returns the name of the notebook
+	 * @return name of notebook
+	 */
 	public String getNotebookName() {
 		return "";
 	}
 
+	/**
+	 * Sets the passed parameter as the current notebook name
+	 * @param name of the notebook
+	 */
 	private void setNotebookName(String name) {
 		
 	}
 	
+	/**
+	 * Returns boolean value of the field isChanged based on whether aspects of 
+	 * the notebook were changed
+	 * @return true if something is changed and false otherwise
+	 */
 	public boolean isChanged() {
 		return true;
 	}
 	
+	/**
+	 * Sets the field isChanged to true if an aspect of the notebook was 
+	 * altered and false otherwise
+	 * @param b true or false
+	 */
 	public void setChanged(boolean b) {
 		
 	}
 	
+	/**
+	 * If the new TaskList’s name is ACTIVE_TASKS_NAME or a duplicate of an
+	 * existing TaskList, then an IAE is thrown. Otherwise, the TaskList is added to the list 
+	 * of task lists, the current task list is updated to the new task list, and isChanged is updated to true.
+	 * @param t specific TaskList passed
+	 */
 	public void addTaskList(TaskList t) {
 		
 	}
 	
+	/**
+	 * Returns a list of task list names. The “Active Tasks” list is always listed first.
+	 * @return Array of strings with taskLists names
+	 */
 	public String[] getTaskListsNames() {
 		return null;
 	}
 	
+	/**
+	 * Iterates through all the TaskLists (in sorted order) 
+	 * and add each active Task (in order of priority). Must clear the ActiveTaskList first
+	 */
 	private void getActiveTaskList() {
 		
 	}
 	
-	public void setCurrentTaskList(String list) {
+	/**
+	 * Sets the string passed as the currentTaskList name
+	 * @param taskListName String passed as parameter
+	 */
+	public void setCurrentTaskList(String taskListName) {
 		
 	}
 	
+	/**
+	 * Returns the new AbstractTaskList as the currentTaskList
+	 * @return AbstractTaskList that is now the current task list
+	 */
 	public AbstractTaskList getCurrentTaskList() {
 		return null;
 	}
 	
-	public void editTaskList(String list) {
+	/**
+	 * Edits a task list name and changes it to the string passed as the parameter
+	 * @param taskListName that the TaskList will be changed to 
+	 */
+	public void editTaskList(String taskListName) {
 		
 	}
 	
+	/**
+	 * CurrentTaskList is removed and then set to the activeTaskList
+	 * isChanged is updated to true.
+	 */
 	public void removeTaskList() {
 		
 	}
 	
+	/**
+	 * A Task can only be added directly to a TaskList. If the currentTaskList is not a TaskList do nothing with the Task. 
+	 * If the currentTaskList is a TaskList, then add the task and check if the Task is active. If so, then you can update the activeTaskList. 
+	 * isChanged is updated to true.
+	 * @param t task added to taskList
+	 */
 	public void addTask(Task t) {
 		
 	}
 	
-	public void editTask(int x, String s, String y, boolean b, boolean p) {
+	/**
+	 * A Task can only be edited if the currentTaskList is a TaskList; otherwise, do nothing. If the Task can be edited, 
+	 * Update the fields of the Task at the specified index. 
+	 * Check if the Task is active. If so, then you can update the activeTaskList... isChanged is updated to true
+	 * @param idx index of task
+	 * @param taskName name of task
+	 * @param taskDescription description of task
+	 * @param recurring whether task is recurring or not
+	 * @param active whether task is active or not
+	 */
+	public void editTask(int idx, String taskName, String taskDescription, boolean recurring, boolean active) {
 		
 	}
 }
--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SortedList.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SortedList.java
@@ -1,36 +1,75 @@
 package edu.ncsu.csc216.wolf_tasks.model.util;
 
+/**
+ * The SortedList class implements the ISortedList interface and extends the Comparable interface. 
+ * The SortedList class uses the Comparable.compareTo() method to determine the ordering of elements
+ * @param <E> general element used in the list
+ * @author Johnathan Howell and
+ */
 public class SortedList<E extends Comparable<E>> implements ISortedList<E> {
 
+	/** size of the list */
 	private int size;
 	
+	/**
+	 * Constructor of SortedList class
+	 */
 	public SortedList() {
 		
 	}
 	
+	/**
+	 * Method to add specific element to the sortedList. Should throw a NullPointerException with message 
+	 * “Cannot add null element.” if the parameter is null. Should throw an IllegalArgumentException 
+	 *  with message “Cannot add duplicate element” if the parameter is a duplicate of an existing element.
+	 *  @param E element to be added to the list
+	 */
 	public void add(E element) {
 		
 	}
 	
+	/**
+	 * Method to remove spefic element from the sorted list at index passed by parameter
+	 * @param idx index that will be removed from the list
+	 * @return E element that is removed
+	 */
 	@SuppressWarnings("unchecked")
 	public E remove(int idx) {
 		return (E) "type";
 	}
 	
+	/**
+	 * Method to check the value that is at the index passed by parameter
+	 * @param idx index that will be check in the list
+	 */
 	private void checkIndex(int idx) {
 		
 	}
 	
+	/**
+	 * Method that checks if the list contains the element that is passed as paramaeter
+	 * @param E element that we are checking for
+	 * @return true if the list contains the element, false otherwise
+	 */
 	public boolean contains(E element) {
 		return true;
 	}
 	
+	/**
+	 * Method that returns the element from the specific index passed as parameter
+	 * @param idx index we are looking at in the list
+	 * @return E element that is at that index
+	 */
 	@SuppressWarnings("unchecked")
 	public E get(int idx) {
 		return (E) "type";
 	}
 	
+	/**
+	 * Method that checks the size of the sortedList
+	 * @return int size 
+	 */
 	public int size() {
-		return 5;
+		return size;
 	}
 }
