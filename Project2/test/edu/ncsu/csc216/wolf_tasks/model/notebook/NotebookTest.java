519d2af06e029f9e8cc8e056092e5351ce9c2c18
Connor Howell <jchowell@ncsu.edu>
2024-04-02 21:00:42 -0400

    Static Analysis NotebookTest

--- Project2/test/edu/ncsu/csc216/wolf_tasks/model/notebook/NotebookTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/notebook/NotebookTest.java
@@ -4,59 +4,94 @@ import static org.junit.Assert.fail;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Test for Notebook class.
+ */
 public class NotebookTest {
 
-	
+	/**
+	 * Tests the Notebook constructor
+	 */
 	@Test
 	public void testNotebookConstructor() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the saveNotebook method
+	 */
 	@Test
 	public void saveNotebookTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the getNotebookName method
+	 */
 	@Test
 	public void getNotebookNameTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the isChanged method
+	 */
 	@Test
 	public void isChangedTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the getTaskListsNames method
+	 */
 	@Test
 	public void getTaskListsNamesTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the setCurrentTaskList method
+	 */
 	@Test
 	public void setCurrentTaskListTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the getCurrentTaskList method
+	 */
 	@Test
 	public void getCurrentTaskListTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the editTaskList method
+	 */
 	@Test
 	public void editTaskListTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the removeTaskList method
+	 */
 	@Test
 	public void removeTaskListTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the addTaskList method
+	 */
 	@Test
 	public void addTaskTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the editTaskList method
+	 */
 	@Test
 	public void editTaskTest() {
 		fail("Not yet implemented");
