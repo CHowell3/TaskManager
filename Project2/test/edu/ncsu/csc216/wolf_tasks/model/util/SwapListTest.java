6b43096544a48d1b85de979f4a020042d6f295da
Connor Howell <jchowell@ncsu.edu>
2024-04-02 20:57:07 -0400

    Static Analysis SwapListTest

--- Project2/test/edu/ncsu/csc216/wolf_tasks/model/util/SwapListTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/util/SwapListTest.java
@@ -4,49 +4,78 @@ import static org.junit.Assert.fail;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests the SwapList class.
+ */
 public class SwapListTest {
 
-	
+	/**
+	 * Tests the SwapList constructor.
+	 */
 	@Test
 	public void testSwapListConstructor() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the add method.
+	 */
 	@Test
 	public void addTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the remove method.
+	 */
 	@Test
 	public void removeTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the moveUp method.
+	 */
 	@Test
 	public void moveUpTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the moveDown method.
+	 */
 	@Test
 	public void moveDownTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the moveToFront method.
+	 */
 	@Test
 	public void moveToFrontTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the moveToBack method.
+	 */
 	@Test
 	public void moveToBackTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the get method.
+	 */
 	@Test
 	public void getTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the size method.
+	 */
 	@Test
 	public void sizeTest() {
 		fail("Not yet implemented");
