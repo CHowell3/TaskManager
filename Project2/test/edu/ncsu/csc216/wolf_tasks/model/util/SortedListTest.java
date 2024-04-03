6a5c89bbc0d484dd605a70557964c0d2216db1ad
Connor Howell <jchowell@ncsu.edu>
2024-04-02 20:56:37 -0400

    Static Analysis SortedListTest

--- Project2/test/edu/ncsu/csc216/wolf_tasks/model/util/SortedListTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/util/SortedListTest.java
@@ -4,34 +4,54 @@ import static org.junit.Assert.fail;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Test for SortedList class.
+ */
 public class SortedListTest {
 
-	
+	/**
+	 * Tests the SortedList constructor.
+	 */
 	@Test
 	public void testSortedListConstructor() {
 		fail("Not yet implemented");
 	}
-	
+
+	/**
+	 * Tests the add method.
+	 */
 	@Test
 	public void addTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the remove method.
+	 */
 	@Test
 	public void removeTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the contains method.
+	 */
 	@Test
 	public void containsTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the get method.
+	 */
 	@Test
 	public void getTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the size method.
+	 */
 	@Test
 	public void sizeTest() {
 		fail("Not yet implemented");
