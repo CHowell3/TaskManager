72bdb206c1dd768dc02c4d4893dcaf64e4e3c73b
Connor Howell <jchowell@ncsu.edu>
2024-04-02 20:57:57 -0400

    Static Analysis TaskTest

--- Project2/test/edu/ncsu/csc216/wolf_tasks/model/tasks/TaskTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/tasks/TaskTest.java
@@ -4,73 +4,117 @@ import static org.junit.Assert.fail;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Test for the Task class.
+ */
 public class TaskTest {
 
+	/**
+	 * Tests the Task constructor.
+	 */
 	@Test
 	public void testTaskConstructor() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the getTaskName method.
+	 */
 	@Test
 	public void getTaskNameTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the setTaskName method.
+	 */
 	@Test
 	public void setTaskNameTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the getTaskDescription method.
+	 */
 	@Test
 	public void getTaskDescriptionTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the setTaskDescription method.
+	 */
 	@Test
 	public void setTaskDescriptionTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the isRecurring method.
+	 */
 	@Test
 	public void isRecurringTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the setRecurring method.
+	 */
 	@Test
 	public void setRecurringTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the isActive method.
+	 */
 	@Test
 	public void isActiveTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the setActive method.
+	 */
 	@Test
 	public void setActiveTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the getTaskListName method.
+	 */
 	@Test
 	public void getTaskListNameTest() {
 		fail("Not yet implemented");
 	}
-	
+	/**
+	 * Tests the addTaskList method.
+	 */
 	@Test
 	public void addTaskListTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the completeTask method.
+	 */
 	@Test
 	public void completeTaskTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the clone method.
+	 */
 	@Test
 	public void cloneTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the toString method.
+	 */
 	@Test
 	public void toStringTest() {
 		fail("Not yet implemented");
