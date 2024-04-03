eecb6b71b18cf74346cd71009e017be1ed1471b0
Connor Howell <jchowell@ncsu.edu>
2024-04-02 20:58:33 -0400

    Static Analysis TaskListTest

--- Project2/test/edu/ncsu/csc216/wolf_tasks/model/tasks/TaskListTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/tasks/TaskListTest.java
@@ -4,19 +4,30 @@ import static org.junit.Assert.fail;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Test for the TaskList class. Since TaskList is a child class of AbstractTaskList, AbstractTaskListTest must also be used to ensure full functionality.
+ */
 public class TaskListTest {
 
-	
+	/**
+	 * Tests the TaskList constructor.
+	 */
 	@Test
 	public void testTaskListConstructor() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the getTasksAsArray method.
+	 */
 	@Test
 	public void getTasksAsArrayTest() {
 		fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests the compareTo method.
+	 */
 	@Test
 	public void compareToTest() {
 		fail("Not yet implemented");
